3d7379df36dc1cf4e0bf735bc7b831c8e5d7d8e6
varshil <132879101+khachariya@users.noreply.github.com>
2023-05-11 13:17:11 +0530

    Add files via upload

---
+++ V-1(11).C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+  clrscr();
+  int a;
+  int b;
+  printf("enter a=");
+  scanf("%d",&a);
+  printf("enter b=");
+  scanf("%d",&b);
+  if(a,b>=40)
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("no");
+  }
+  getch();
+}
